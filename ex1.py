86496073ca9e9f776df17434d1336d53719dcf6c
Ricardo Conchas <ricardoconchas@gmail.com>
2021-11-04 12:56:45 -0600

    ex1 double array not nested to give O(N) time

---
+++ ex1.py
@@ -0,0 +1,14 @@
+arr = [1,2,3,4,5]
+
+def foo(arr):
+    sum = 0
+    product = 1
+    for i in range (0,len(arr)):
+        sum += arr[i]
+    for i in range(0,len(arr)):
+        product *= arr[i]
+    print(f"{sum} , {product}")
+
+foo(arr)
+
+# This will take O(N) time. The fact that we iterate though the array  twice doesn't matter
